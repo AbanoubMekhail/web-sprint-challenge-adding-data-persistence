071793954a2d1d5ff60aa9362495115642af9845
abanoub nagib <abanoub.nagib@hotmail.com>
2023-11-15 02:27:30 -0800

    resource router&model

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,39 @@
 // build your `/api/resources` router here
+const express = require("express");
+const Resources = require("./model");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Resources.getResources()
+    .then((resources) => {
+      res.status(200).json(resources);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "Error retrieving resources" });
+    });
+});
+
+router.post("/", (req, res) => {
+  const resourceData = req.body;
+
+  Resources.addResource(resourceData)
+    .then((resource) => {
+      res.status(201).json(resource);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "Error adding resource" });
+    });
+});
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        customMessage: 'somthing went wrong inside the project router',
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router;
